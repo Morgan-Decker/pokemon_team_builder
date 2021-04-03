e2792138a6ac777209a1113942cd546b702a2332
holly-hewitt <77455575+holly-hewitt@users.noreply.github.com>
2021-04-03 18:32:25 +0100

    added friends field

--- builder/models.py
+++ builder/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    friends = models.ManyToManyField('self')
 
     def __str__(self):
         return self.user.username
