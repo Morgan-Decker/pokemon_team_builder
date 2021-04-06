58558c13f7280bac90e0f6730050028cdd685e34
Craig Livingstone <2470470l@student.gla.ac.uk>
2021-04-06 19:02:54 +0100

    bug fix

--- builder/tests.py
+++ builder/tests.py
@@ -1,11 +1,24 @@
 from django.test import TestCase
 from builder.models import Team
+from django.urls import reverse
 
 # Create your tests here.
 class TeamMethodTests(TestCase):
     def test_ensure_views_are_positive(self):
-        """Ensures the number of views received for a Category are positive or zero."""
+        """Ensures the number of likes received for a team are positive or zero."""
         team=Team(teamname='test', likes=-1, userprofile="test")
         team.save()
 
         self.assertEqual((team.likes >= 0), True)
+
+class ViewTests(TestCase):
+    def test_home_view_with_no_teams(self):
+        response=self.client.get(reverse('builder:home'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, '--- There are no teams yet, better get building! ---')
+
+    def test_teamview_view_with_no_teams(self):
+        """If no categories exist, the appropriate message should be displayed."""
+        response = self.client.get(reverse('builder:team_view'))
+        self.assertEqual(response.status_code , 200)
+        # self.assertContains(response, '--- There are no teams yet, better get building! ---')
--- pokemon_team_builder/settings.py
+++ pokemon_team_builder/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'builder.apps.BuilderConfig',
+    'builder.apps.BuilderConfig'
 ]
 
 MIDDLEWARE = [
